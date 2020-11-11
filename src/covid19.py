894f30136f90a026b5ac00155f53a2524fc07571
SamuelTribeUK <SamuelTribeUK@outlook.com>
2020-11-11 18:16:56 +0000

    Implemented DateType casting to date column

--- src/covid19.py
+++ src/covid19.py
@@ -7,6 +7,7 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.session import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import DateType
 
 # The spark session is configured here
 conf = SparkConf().setAppName("covid19").setMaster("local")
@@ -29,6 +30,7 @@ separatorText("Coursework 1 Big Data Analytics - Samuel Tribe - 201318996")
 # The covid19.csv file is read with header=True and inferSchema=True (to ensure
 # correct dataTypes)
 covidDF = spark.read.csv(csvPath,header=True,inferSchema=True)
+covidDF = covidDF.withColumn("date", F.col("date").cast(DateType()))
 # The dataframe is shown and the schema is printed to show the data has been
 # loaded correctly
 separatorText("covid19.csv read as Dataframe with header=True")
